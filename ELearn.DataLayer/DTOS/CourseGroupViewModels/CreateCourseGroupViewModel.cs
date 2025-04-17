ccfcff67b4db0005ad5eaf59459a1fd98ea4bf80
Amin87Robin <amin87eft@gmail.com>
2025-04-17 12:38:59 +0330

    Add View

---
+++ ELearn.DataLayer/DTOS/CourseGroupViewModels/CreateCourseGroupViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELearn.DataLayer.DTOS.CourseGroupViewModels
+{
+    public class CreateCourseGroupViewModel
+    {
+        [Required(ErrorMessage ="مقدار نام گروه نمیتواند خالی باشد")]
+        [DisplayName("نام گروه ")]
+        [MaxLength(15, ErrorMessage ="مقدار کارکتر ها باید نهایتا پانزده تا باشد")]
+        public string CourseGroupName { get; set; }
+    }
+}
--- ELearn/Areas/admin/Controllers/CourseGroupsController.cs
+++ ELearn/Areas/admin/Controllers/CourseGroupsController.cs
@@ -1,4 +1,5 @@
 ﻿using ELearn.Core.Interfaces;
+using ELearn.DataLayer.DTOS.CourseGroupViewModels;
 using ELearn.DataLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,8 +16,33 @@ namespace ELearn.Areas.admin.Controllers
         }
         public IActionResult Index()
         {
-            List<CourseGroup> courseGroups = _courseGroupServices.GetCourseGroup().ToList();
+            List<CourseGroup> courseGroups = _courseGroupServices.GetCourseGroups().ToList();
             return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCourseGroup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCourseGroup(CreateCourseGroupViewModel createCourseGroup)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(createCourseGroup);
+            }
+            CourseGroup courseGroup = new CourseGroup
+            {
+                CourseGroupName = createCourseGroup.CourseGroupName,
+
+            };
+            if (!await _courseGroupServices.CreateCourseGroup(courseGroup))
+            {
+                TempData["Error"] = "عملیات با موفقیت شکست خورد !";
+                return RedirectToAction("index");
+            }
+            TempData["Success"] = "عملیات با موفقیت پیروز شد !";
+            return RedirectToAction("index");
         }
     }
 }
